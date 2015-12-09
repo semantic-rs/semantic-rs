8a4fb449a5db41a539f2738e5f5ce9d3804d555a
Jan Schulte <hello@unexpected-co.de>
2015-12-09 20:16:52 +0100

    feat(main): Include logger in main module

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,23 @@
 mod git_history;
 mod git_client;
+mod logger;
+
+fn print_log(log_entries: Vec<git_history::LogEntry>) {
+    for entry in log_entries {
+        logger::stdout(entry.revision);
+        logger::stdout(entry.title);
+    }
+}
 
 fn main() {
     println!("semantic.rs 🚀");
+
+    logger::stdout("Fetched git history");
+    match git_client::log() {
+        Ok(log) => print_log(log),
+        Err(err) => {
+            logger::stderr("fatal: Failed to retrieve git history");
+            logger::stderr(err);
+        }
+    }
 }
