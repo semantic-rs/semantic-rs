bc3e1351bcebe831aaa5ee69a07477ccdc6a3113
Jan Schulte <hello@unexpected-co.de>
2015-12-28 09:32:40 -0500

    feat(toml_file): add read and write functions for Cargo.toml

--- src/toml_file.rs
+++ src/toml_file.rs
@@ -1,5 +1,13 @@
 use toml::Parser;
 use regex::Regex;
+use std::io::prelude::*;
+use std::fs::File;
+use std::io::Error;
+
+pub enum TomlError {
+    parseError(&'static str),
+    ioError(Error)
+}
 
 pub fn read_version(file: String) -> Option<String> {
    let file_map = Parser::new(&file).parse().unwrap();
@@ -18,6 +26,53 @@ pub fn file_with_new_version(file: String, new_version: &str) -> String {
     let re = Regex::new(r#"version\s=\s"\d+\.\d+\.\d+""#).unwrap();
     let new_version = format!("version = \"{}\"", new_version);
     re.replace(&file, &new_version[..])
+}
+
+pub fn read_from_file() -> Result<String, TomlError> {
+    let cargo_file = match read_cargo_toml() {
+        Ok(buffer) => buffer,
+        Err(err) => return Err(TomlError::ioError(err))
+    };
+
+    match read_version(cargo_file) {
+        Some(version) => Ok(version),
+        None => Err(TomlError::parseError("No version field found"))
+    }
+}
+
+pub fn write_new_version(new_version: String) -> Result<(), Error> {
+    let cargo_toml = match read_cargo_toml() {
+        Ok(file) => file,
+        Err(err) => return Err(err)
+    };
+    let new_cargo_toml = file_with_new_version(cargo_toml, &new_version);
+
+    let mut handle = match File::open("Cargo.toml") {
+        Ok(handle) => handle,
+        Err(err) => {
+            return Err(err)
+        }
+    };
+
+    match handle.write_all(new_cargo_toml.as_bytes()) {
+        Ok(_) => Ok(()),
+        Err(err) => Err(err)
+    }
+}
+
+fn read_cargo_toml() -> Result<String, Error> {
+    let mut handle = match File::open("Cargo.toml") {
+        Ok(handle) => handle,
+        Err(err) => {
+            return Err(err)
+        }
+    };
+
+    let mut buffer = String::new();
+    match handle.read_to_string(&mut buffer) {
+        Ok(_) => Ok(buffer),
+        Err(err) => Err(err)
+    }
 }
 
 #[cfg(test)]
