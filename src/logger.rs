5ea38eebcf882eb9b87df3e827f62cc247ed388b
Jan Schulte <hello@unexpected-co.de>
2016-01-23 21:21:22 +0100

    refactor: Remove unused success logger function

--- src/logger.rs
+++ src/logger.rs
@@ -4,16 +4,11 @@ use std::io::prelude::*;
 
 pub enum MessageType {
     Info,
-    Success,
     Error
 }
 
 pub fn stdout<P: AsRef<str>>(message: P) {
     print_message(message, MessageType::Info);
-}
-
-pub fn success<P: AsRef<str>>(message: P) {
-    print_message(message, MessageType::Success);
 }
 
 pub fn stderr<P: AsRef<str>>(message: P) {
@@ -24,12 +19,6 @@ fn print_message<P: AsRef<str>>(message: P, message_type: MessageType) {
     match message_type {
         MessageType::Info => {
             println!("{}", message.as_ref());
-        },
-        MessageType::Success => {
-            let mut success_terminal = term::stdout().unwrap();
-            success_terminal.fg(term::color::GREEN).unwrap();
-            writeln!(success_terminal, "{}", message.as_ref()).unwrap();
-            success_terminal.reset().unwrap();
         },
         MessageType::Error => {
             let mut error_terminal = term::stderr().unwrap();
