d075f30a239ce6853de4d783a1b64efa654500b6
Jan-Erik Rediger <janerik@fnordig.de>
2016-03-09 17:02:48 +0100

    feat: Handle IoError

--- src/error.rs
+++ src/error.rs
@@ -1,6 +1,7 @@
 use git2::Error as GitError;
 use std::env::VarError;
 use std::error::Error as StdError;
+use std::io::Error as IoError;
 
 use std::fmt;
 use std::convert::From;
@@ -11,6 +12,7 @@ use self::Error::*;
 pub enum Error {
     Git(GitError),
     Var(VarError),
+    Io(IoError),
 }
 
 impl From<GitError> for Error {
@@ -25,11 +27,18 @@ impl From<VarError> for Error {
     }
 }
 
+impl From<IoError> for Error {
+    fn from(err: IoError) -> Error {
+        Error::Io(err)
+    }
+}
+
 impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match *self {
             Git(ref e) => e.fmt(f),
             Var(ref e) => e.fmt(f),
+            Io(ref e) => e.fmt(f),
         }
 
     }
@@ -40,6 +49,7 @@ impl StdError for Error {
         match *self {
             Git(ref e) => e.description(),
             Var(ref e) => e.description(),
+            Io(ref e) => e.description(),
         }
     }
 }
