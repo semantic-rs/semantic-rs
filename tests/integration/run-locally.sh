a3f0c9e65a2416a9a4a7bb9b776e7da456d9b2a9
Jan-Erik Rediger <janerik@fnordig.de>
2016-08-28 19:55:39 +0200

    fix: Avoid non-POSIX readlink parameter
    Fixes #103

--- tests/integration/run-locally.sh
+++ tests/integration/run-locally.sh
@@ -2,12 +2,17 @@
 
 set -e
 
-cd $(dirname $(readlink -f $0))
+expandpath() {
+  CDPATH= cd -- "$1" && pwd -P
+}
 
-export WORKSPACE=$(readlink -f ../../..)/workspace
+SCRIPTPATH=$(expandpath "$(dirname -- $0)")
+cd "$SCRIPTPATH"
+
+export WORKSPACE="$(expandpath ../../..)/workspace"
 
 git clone https://github.com/sstephenson/bats/ || true
-export PATH=$(pwd)/bats/bin:$(readlink -f ../../target/debug):$PATH
+export PATH=$(pwd)/bats/bin:$(expandpath ../../target/debug):$PATH
 
 rm -rf $WORKSPACE
 mkdir $WORKSPACE
