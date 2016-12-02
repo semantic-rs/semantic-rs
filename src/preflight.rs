8e248bb47431113616c61bc09b91f10d8ce1e72f
Jan Schulte <hello@unexpected-co.de>
2016-12-02 22:30:57 +0100

    feat: Check if CARGO_TOKEN is defined or not

--- src/preflight.rs
+++ src/preflight.rs
@@ -7,6 +7,10 @@ pub fn check(config: &Config) -> Vec<String> {
         warnings.push("The GH_TOKEN environment variable is not configured".into());
     }
 
+    if config.cargo_token.is_none() {
+        warnings.push("The CARGO_TOKEN environment variable is not configured. Cannot create release on crates.io".into());
+    }
+
     if let Err(ref err) = config.remote {
         warnings.push(format!("Could not determine the origin remote url: {:?}", err));
         warnings.push("semantic-rs can't push changes or create a release on GitHub".into());
