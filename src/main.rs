09e2f8673bc903a6e77a72357e0dc392a7cc3787
mersinvald <me@mkl.dev>
2019-07-06 21:44:48 +0300

    fix: remove travis-specific build order checks

--- src/main.rs
+++ src/main.rs
@@ -23,7 +23,6 @@ use std::process;
 use std::thread;
 use std::time::Duration;
 use std::{env, fs};
-use travis_after_all::Build;
 use utils::user_repo_from_url;
 
 const VERSION: &'static str = env!("CARGO_PKG_VERSION");
@@ -419,26 +418,6 @@ fn main() {
 
     for warning in warnings {
         log::warn!("{}", warning);
-    }
-
-    if config.release_mode && ci_env_set() {
-        let build_run = Build::from_env().unwrap_or_else(|e| {
-            print_exit!("CI mode, but can't check other builds. Error: {:?}", e)
-        });
-
-        if !build_run.is_leader() {
-            log::info!("Not the build leader. Nothing to do. Bye.");
-            process::exit(0);
-        }
-
-        log::info!("I am the build leader. Waiting for other jobs to finish.");
-        match build_run.wait_for_others() {
-            Ok(()) => log::info!("Other jobs finished and succeeded. Doing my work now."),
-            Err(travis_after_all::Error::FailedBuilds) => {
-                print_exit!("Some builds failed. Stopping here.");
-            }
-            Err(e) => print_exit!("Waiting for other builds failed. Reason: {:?}", e),
-        }
     }
 
     let version = toml_file::read_from_file(&config.repository_path)
